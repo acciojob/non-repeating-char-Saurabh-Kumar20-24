476407134fd3a191d027c191e59ac85889b6d14c
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-04-06 17:32:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let obj={};
+	for(let char of str){
+		obj[char]=(obj[char]||0) + 1;
+	}
+	for(let [key,value] of Object.entries(obj)){
+		if(value===1) return key;
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
